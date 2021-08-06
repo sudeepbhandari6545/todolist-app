ff2080aea3fa6d94374ecd296209abdce1b4d473
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-08-06 19:34:01 +0545

    todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 //
 import AddIcon from '@material-ui/icons/Add'
 import EditIcon from '@material-ui/icons/Edit'
 import DeleteIcon from '@material-ui/icons/Delete'
 import './style.css'
 
+const getLocalData = () => {}
 const TodoList = () => {
+  const [inputData, setInputData] = useState('')
+  const [items, setItems] = useState(getLocalData())
+
+  const addItem = () => {
+    if (!inputData) {
+      alert('input the item')
+    } else {
+      const myNewData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      }
+      setItems([...items, myNewData])
+      setInputData('')
+    }
+  }
+  const deletItem = (index) => {
+    const updateItem = items.filter((curElement) => {
+      return curElement.id !== index
+    })
+    setItems(updateItem)
+  }
+  const removeAll = () => {
+    setItems([])
+  }
+  useEffect(() => {
+    localStorage.setItem('mytodolist', JSON.stringify(items))
+  }, [items])
+
   return (
     <>
       <div className="main_div">
@@ -20,17 +49,36 @@ const TodoList = () => {
           </figure>
           <p>Add your list here</p>
           <div className="list_input">
-            <input type="text" placeholder="Enter the List" />
-            <AddIcon className="plus" />
-            <div className="input_item">
-              <p>
-                Apple
-                <EditIcon className="edit" />
-                <DeleteIcon className="delet" />
-              </p>
-            </div>
+            <input
+              type="text"
+              placeholder="Enter the List"
+              value={inputData}
+              onChange={(event) => setInputData(event.target.value)}
+            />
+            <AddIcon className="plus" onClick={addItem} />
+            {/* map method */}
+            {items.map((curElement, index) => {
+              return (
+                <div className="input_item" key={curElement.id}>
+                  <p>
+                    {curElement.name}
+                    <EditIcon className="edit" />
+                    <DeleteIcon
+                      className="delet"
+                      onClick={() => deletItem(curElement.id)}
+                    />
+                  </p>
+                </div>
+              )
+            })}
           </div>
-          <button className="my_button">Add list</button>
+          <button
+            className="my_button"
+            data-sm-link-text="Remove All"
+            onClick={removeAll}
+          >
+            Delete All
+          </button>
         </div>
       </div>
     </>
